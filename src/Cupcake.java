9fb69234c5591a01860d16f520e41bbf441d7872
Adityan M V <adithyanmv2006@gmail.com>
2026-01-29 19:35:41 +0530

    error updated

--- src/Cupcake.java
+++ src/Cupcake.java
@@ -1,24 +1,41 @@
 import java.util.*;
-
 public class Cupcake {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        Integer[] calories = new Integer[n];
-
-        for (int i = 0; i < n; i++) {
-            calories[i] = sc.nextInt();
+        public static void bubbleSort(int n,int arr[]){
+                for(int i=0;i<n;i++){
+                        for(int j=0;j<n-i-1;j++){
+                                if(arr[j]<arr[j+1]){
+                                        int temp=arr[j];
+                                        arr[j]=arr[j+1];
+                                        arr[j+1]=temp;
+                                }
+                        }
+                }
         }
-
-        Arrays.sort(calories, Collections.reverseOrder());
-
-        long miles = 0;
-        for (int i = 0; i < n; i++) {
-            miles += (long) calories[i] * (1L << i);
+        public static long Miles(int n,int arr[]){
+                long calorie=0;
+                long multi=1;
+                for(int i=0;i<n;i++){
+                        calorie=calorie+arr[i]*multi;
+                        multi*=2;
+                }
+                return calorie;
         }
-
-        System.out.println(miles);
-        sc.close();
-    }
+        public static void main(String args[]){
+                Scanner sc=new Scanner(System.in);
+                int n=sc.nextInt();
+                int arr[]=new int[n];
+                for(int i=0;i<n;i++){
+                        arr[i]=sc.nextInt();
+                        
+                }
+                bubbleSort(n,arr);
+                long result=Miles(n,arr);
+                System.out.print(result);                
+        }
+        // TODO: Read n
+        // TODO: Read calorie counts into an array
+        // TODO: Sort the array (think about ascending vs descending)
+        // TODO: Calculate the minimum miles using the formula (c * 2^j)
+        // TODO: Print the result (Use 'long' for the sum to avoid overflow)
+    
 }
